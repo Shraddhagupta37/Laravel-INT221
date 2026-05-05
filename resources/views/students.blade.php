97b5e23cae7627d13207e6be0a0e432d77675021
Shraddhagupta37 <gshraddha246850@gmail.com>
2026-05-05 15:12:37 +0530

    Save

--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -2,7 +2,7 @@
     <!-- I begin to speak only when I am certain what I will say is not better left unsaid. - Cato the Younger -->
     <h1>Student List</h1>
     <table border="1" cellpadding="10">
-        <tr>
+        <tr style="background-color: lightgray;">
             <th>Reg No</th>
             <th>Name</th>
             <th>Age</th>
@@ -11,7 +11,7 @@
             <th>CGPA</th>
         </tr>
         @foreach($students as $student)
-        <tr>
+        <tr style="background-color: {{ $student->cgpa < 8 ? '#ffebee' : '#e8f5e9'  }};">
             <td>{{ $student->regno }}</td>
             <td>{{ $student->name }}</td>
             <td>{{ $student->age }}</td>    
